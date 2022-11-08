a61aa29e7c3412943fc640615351fc6192393a13
Ronald George <ronaldsamgeorge@gmail.com>
2022-11-08 14:20:52 +0530

    Feature : Provide Feedback  and Display Summary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Display = (props) => {
+  return(
+    <div>
+      <h1>{props.text}</h1>
+    </div>
+  )
+}
+
+const Button = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <button onClick={props.onClick}>{props.text}</button>
+    </>
+  )
+}
+
+const DisplayResult = (props) => {
+  const {text, rating} = props;
+  return(
+    <div>
+      <p>{text} : {rating}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+
+  const [goodRating, setGood] = useState(0);
+  const [badRating, setBad] = useState(0);
+  const [neutralRating, setNeutral] = useState(0);
+
+  const updateGood = () => setGood(goodRating + 1);
+  
+  const updateBad = () => setBad(badRating + 1);
+
+  const updateNeutral = () => setNeutral(neutralRating + 1);
+  
+  
+  return (
+    <div>
+      <Display text={"Give Feedback"}/>
+      <Button onClick={updateGood} text="Good" />
+      <Button onClick={updateNeutral} text="Neutral" />
+      <Button onClick={updateBad} text="Bad" />
+
+      <Display text={"Summary"} />
+      <DisplayResult text="Good" rating={goodRating} />
+      <DisplayResult text="Neutal" rating={neutralRating} />
+      <DisplayResult text="Bad" rating={badRating} />
+      
     </div>
   );
 }
